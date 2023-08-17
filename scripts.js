6ca9aa03a289b4c69e33b1666f323123984f5d01
Thiago Fontanella <thiago.fontanella@partner.sdslasupport.com>
2023-08-16 22:44:42 -0300

    feature: putting real data on the project

--- scripts.js
+++ scripts.js
@@ -5,7 +5,7 @@ function toggleMode() {
   const img = document.querySelector("#profile img")
 
   if (html.classList.contains("light")) {
-    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("src", "./assets/avatar-light.jpg")
     img.setAttribute("alt", "Foto de perfil do usuário modo claro")
   } else {
     img.setAttribute("src", "./assets/avatar.png")
